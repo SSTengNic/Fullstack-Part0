298886c0e911f77827b323856cc239c00c717ddf
SSTengNic <94430649+SSTengNic@users.noreply.github.com>
2022-08-15 18:55:28 +0800

    Add files via upload

---
+++ part 7/ex 7.6-7.8/src/App.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState([])
+
+  useEffect (()=> {
+    axios.get(`https://restcountries.com/v2/name/${name}?fullText=true`)
+      .then(response=> {
+        setCountry(response.data)
+      })
+      .catch(
+        setCountry([])
+        )
+  },[name])
+  
+  useEffect(() => {
+    setCountry([])
+  }, [])
+  
+  return country
+}
+
+const Country = ({ country }) => {
+
+  if (country.length===0) {
+    return <div>not found...</div>
+  }
+
+  return (
+    <div>
+      <h3>{country[0].name}</h3>
+      <div>population {country[0].population}</div> 
+      <div>capital {country[0].capital}</div>
+      <img src={country[0].flags.png} height='100' alt={`flag of ${country[0].name}`}/> 
+    </div>
+  )  
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App

e07d101ab112fa30ceea91d78e267c825f785ce9
SSTengNic <94430649+SSTengNic@users.noreply.github.com>
2022-08-30 21:50:53 +0800

    Add files via upload

---
+++ part 5/cypress/e2e/note_app.spec.cy.js
@@ -0,0 +1,67 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('login fails with wrong password', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'Wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+  }),
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
+  }),
+
+  it('log form can be opened', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Matti Luukkainen logged-in')
+
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+      /*cy.login({ username: 'mluukkai', password: 'salainen' })
+      cy.contains('login').click()
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()*/
+    })
+
+    describe('create new note', function() {
+      beforeEach(function() {
+        cy.login({ username: 'mluukkai', password: 'salainen' })
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
+      })
+
+      it('importance toggle test', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
+      })
+    })
+  })
+})
